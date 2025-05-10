36a96442bb91edbff6dc7eee57e176ff9d3bdd37
Techno India <133761719+TechnoIndian@users.noreply.github.com>
2025-05-10 18:58:09 +0530

    Add files via upload

--- RKPairip/Anti_Splits.py
+++ RKPairip/Anti_Splits.py
@@ -4,16 +4,18 @@ from.Files_Check import FileCheck
 F=FileCheck()
 F.set_paths()
 def Anti_Split(apk_path,isMerge):
-	A=apk_path
+	G=isMerge;A=apk_path
 	try:
-		E,G=C.os.path.splitext(A)
-		if G in['.apks','.apkm','.xapk']:
-			print(f"\n{C.r}_____________________________________________________________\n");print(f"\n{C.lb}[ {C.pr}* {C.lb}] {C.c} Anti-Split Start...");B=f"{E.replace(' ','_')}.apk";print(f"{C.g}  |\n  └──── {C.r}Decompiling ~{C.g}$ java -jar {C.os.path.basename(F.apkeditor_path)} m -i {A} -f -o {B}\n");print(f"{C.r}_____________________________________________________________{C.g}\n");H=['java','-jar',F.apkeditor_path,'m','-i',A,'-f','-o',B]
+		I,H=C.os.path.splitext(A);B=['.apks','.apkm','.xapk']
+		if H in B:
+			print(f"{C.r}_____________________________________________________________\n");print(f"\n{C.lb}[ {C.pr}* {C.lb}] {C.c} Anti-Split Start...");D=f"{I.replace(' ','_')}.apk";print(f"{C.g}  |\n  └──── {C.r}Decompiling ~{C.g}$ java -jar {C.os.path.basename(F.APKEditor_Path)} m -i {A} -f -o {D}\n");print(f"{C.r}_____________________________________________________________{C.g}\n");J=['java','-jar',F.APKEditor_Path,'m','-i',A,'-f','-o',D]
 			try:
-				I=C.subprocess.run(H,check=True);print(f"\n{C.lb}[ {C.pr}* {C.lb}] {C.c} Anti-Split Successful  {C.g}✔{C.r}\n");print(f"{C.r}_____________________________________________________________\n")
-				if isMerge:exit()
-				return B
-			except C.subprocess.CalledProcessError as D:exit(f"\n{C.lb}[ {C.rd}Error ! {C.lb}] {C.rd} Anti-Split Failed ! ✘{C.r}\n\n{C.lb}[ {C.rd}Error ! {C.lb}] {C.rd} Error Output: {D.stderr}\n")
-		else:print(f"\n{C.lb}[{C.c} Info {C.lb}] {C.rd}Split ✘{C.r}\n")
+				K=C.subprocess.run(J,check=True);print(f"\n{C.lb}[ {C.pr}* {C.lb}] {C.c} Anti-Split Successful  {C.g}✔{C.r}\n");print(f"{C.r}_____________________________________________________________\n")
+				if G:exit()
+				return D
+			except C.subprocess.CalledProcessError as E:exit(f"\n{C.lb}[ {C.rd}Error ! {C.lb}] {C.rd} Anti-Split Failed ! ✘{C.r}\n\n{C.lb}[ {C.rd}Error ! {C.lb}] {C.rd} Error Output: {E.stderr}\n")
+		if G and H not in B:exit(f"""
+{C.lb}[{C.c} Info {C.lb}] {C.rd}Split ✘{C.r}\n\n\n{C.lb}[ {C.pr}* {C.lb}] {C.c} Only Supported Extensions {C.g}{B}
+""")
 		return A
-	except(FileNotFoundError,Exception)as D:exit(f"\n{C.lb}[ {C.rd}Error ! {C.lb}] {C.rd} {str(D)} ✘{C.r}\n")
+	except(FileNotFoundError,Exception)as E:exit(f"\n{C.lb}[ {C.rd}Error ! {C.lb}] {C.rd} {str(E)} ✘{C.r}\n")
--- RKPairip/Files_Check.py
+++ RKPairip/Files_Check.py
@@ -1,7 +1,7 @@
 from.C_M import CM
 C=CM()
 class FileCheck:
-	def set_paths(A):B=C.os.path.dirname(C.os.path.abspath(C.sys.argv[0]));A.apkeditor_path=C.os.path.join(B,'APKEditor.jar');A.apktool_path=C.os.path.join(B,'APKTool_OR.jar');A.axml2xml_jar_path=C.os.path.join(B,'axml2xml.jar');A.Objectlogger=C.os.path.join(C.os.path.dirname(C.os.path.abspath(__file__)),'Objectlogger.smali');A.baksmali=C.os.path.join(C.os.path.dirname(C.os.path.abspath(__file__)),'Bak_Smali.jar')
+	def set_paths(A):B=C.os.path.dirname(C.os.path.abspath(C.sys.argv[0]));A.APKEditor_Path=C.os.path.join(B,'APKEditor.jar');A.APKTool_Path=C.os.path.join(B,'APKTool_OR.jar');A.Axml2Xml_Path=C.os.path.join(B,'Axml2Xml.jar');A.Objectlogger=C.os.path.join(C.os.path.dirname(C.os.path.abspath(__file__)),'Objectlogger.smali');A.baksmali=C.os.path.join(C.os.path.dirname(C.os.path.abspath(__file__)),'Bak_Smali.jar')
 	def calculate_checksum(E,file_path):
 		A=C.hashlib.sha256()
 		try:
@@ -9,30 +9,30 @@ class FileCheck:
 				for D in iter(lambda:B.read(4096),b''):A.update(D)
 			return A.hexdigest()
 		except FileNotFoundError:return
-	def download_file(K,jar_urls_and_paths):
+	def download_file(K,Jar_Files):
 		H=True;import requests as F;Z=set()
-		for(L,A,M)in jar_urls_and_paths:
+		for(L,A,M)in Jar_Files:
 			B=C.os.path.basename(A)
 			if C.os.path.exists(A):
 				N=K.calculate_checksum(A)
 				if N==M:continue
 				else:print(f"{C.rd}[ {C.pr}File {C.rd}] {C.c}{B} {C.rd}is Corrupt (Checksum Mismatch).\n\n{C.lb}[ {C.y}INFO ! {C.lb}]{C.rd} Re-Downloading, Need Internet Connection.{C.r}\n");C.os.remove(A)
 			try:
-				O='2.2';P='https://raw.githubusercontent.com/TechnoIndian/ApkPatcher/main/setup.py'
+				O='2.0';P='https://raw.githubusercontent.com/TechnoIndian/RK_Pairip/main/setup.py'
 				if(Q:=F.get(P)).status_code==200 and(R:=C.re.search('version="(.*?)"',Q.text)):
-					if(S:=R.group(1))!=O:print(f"\n{C.lb}[ {C.y}Update {C.lb}]{C.c} Updating ApkPatcher to {C.g}{S}...\n\n");I=['pip','install','git+https://github.com/TechnoIndian/ApkPatcher.git']if C.os.name=='nt'else'curl -L -o ApkPatcher.sh https://github.com/TechnoIndian/ApkPatcher/releases/download/ApkPatcher/ApkPatcher.sh && chmod +x ApkPatcher.sh && ./ApkPatcher.sh';C.subprocess.run(I,shell=isinstance(I,str),check=H)
+					if(S:=R.group(1))!=O:print(f"\n{C.lb}[ {C.y}Update {C.lb}]{C.c} Updating RK_Pairip to {C.g}{S}...\n\n");I=['pip','install','git+https://github.com/TechnoIndian/RK_Pairip.git']if C.os.name=='nt'else'curl -L -o RKPairip.sh https://github.com/TechnoIndian/RK_Pairip/releases/download/RKPairip/RKPairip.sh && chmod +x RKPairip.sh && ./RKPairip.sh';C.subprocess.run(I,shell=isinstance(I,str),check=H)
 				print(f"\n{C.lb}[ {C.pr}Downloading {C.lb}] {C.c}{B}",end='',flush=H);D=F.get(L,stream=H,timeout=10)
 				if D.status_code==200:
 					E=int(D.headers.get('content-length',0));T=1024;G=0
 					with open(A,'wb')as U:
 						for J in D.iter_content(T):G+=len(J);U.write(J);V=G/E*100 if E>0 else 0;W=G/(1024*1024);X=E/(1024*1024)if E>0 else 0;Y=f"\r{C.lb}[ {C.pr}Downloading {C.lb}] {C.c}{B} {C.g}➸❥ {V:.2f}% ({W:.2f}/{X:.2f} MB)";print(Y,end='\r')
 					print(f"\n{C.g}       |\n       └──── {C.r}Downloaded ~{C.g}$ {B} Successfully. ✔\n")
-				else:exit(f'\n\n{C.lb}[ {C.rd}Error ! {C.lb}]{C.rd} Failed to download {C.y}{B} {C.rd}Status Code: {D.status_code}\n\n{C.lb}[ {C.y}INFO ! {C.lb}]{C.rd} Restart Script...{C.r}\n')
-			except F.exceptions.RequestException:exit(f'\n\n{C.lb}[ {C.rd}Error ! {C.lb}]{C.rd} Got an error while Fetching {C.y}{A}\n\n{C.lb}[ {C.rd}Error ! {C.lb}]{C.rd} No internet Connection\n\n{C.lb}[ {C.y}INFO ! {C.lb}]{C.rd} Internet Connection is Required to Download {C.y}{B}\n')
-	def F_D(A):B=[('https://github.com/TechnoIndian/RKPairip/releases/download/Editor/APKEditor.jar',A.apkeditor_path,'758f2f9153fff96c20260b177f025a3ca3cecc9777abdd43139a17e225724612'),('https://raw.githubusercontent.com/TechnoIndian/Objectlogger/main/Objectlogger.smali',A.Objectlogger,'ff31dd1f55d95c595b77888b9606263256f1ed151a5bf5706265e74fc0b46697')];A.download_file(B);C.os.system('cls'if C.os.name=='nt'else'clear')
+				else:exit(f"""\n\n{C.lb}[ {C.rd}Error ! {C.lb}]{C.rd} Failed to download {C.y}{B} {C.rd}Status Code: {D.status_code}\n\n{C.lb}[ {C.y}INFO ! {C.lb}]{C.rd} Restart Script...{C.r}\n""")
+			except F.exceptions.RequestException:exit(f"""\n\n{C.lb}[ {C.rd}Error ! {C.lb}]{C.rd} Got an error while Fetching {C.y}{A}\n\n{C.lb}[ {C.rd}Error ! {C.lb}]{C.rd} No internet Connection\n\n{C.lb}[ {C.y}INFO ! {C.lb}]{C.rd} Internet Connection is Required to Download {C.y}{B}\n""")
+	def F_D(A):B=[('https://github.com/TechnoIndian/Tools/releases/download/Tools/APKEditor.jar',A.APKEditor_Path,'d4280b36fc78ba1f11c04a8a9d91fa8646cf4c41e96fd0addc9afb81b78dcbe9'),('https://raw.githubusercontent.com/TechnoIndian/Objectlogger/main/Objectlogger.smali',A.Objectlogger,'ff31dd1f55d95c595b77888b9606263256f1ed151a5bf5706265e74fc0b46697')];A.download_file(B);C.os.system('cls'if C.os.name=='nt'else'clear')
 	def F_D_A(B,isAPKTool,Fix_dex,isSmali):
 		E=isSmali;D=Fix_dex;C=isAPKTool;A=[]
-		if C or D:A.append(('https://github.com/TechnoIndian/RKPairip/releases/download/Editor/apktool.jar',B.apktool_path,'56d59c524fc764263ba8d345754d8daf55b1887818b15cd3b594f555d249e2db'))
-		if E:A.append(('https://github.com/TechnoIndian/ApkTool_Framwork/releases/download/APKTool_2.10.0/Bak_Smali.jar',B.baksmali,'418b3861efc0f3afffb10d58a244e848d34f86cbdd3261ad2a2d1d57ee663766'))
-		if C or D or E:A.append(('https://github.com/TechnoIndian/RKPairip/releases/download/Editor/axml2xml.jar',B.axml2xml_jar_path,'e3a09af1255c703fc050e17add898562e463c87bb90c085b4b4e9e56d1b5fa62'))
+		if C or D:A.append(('https://github.com/TechnoIndian/Tools/releases/download/Tools/APKTool.jar',B.APKTool_Path,'56d59c524fc764263ba8d345754d8daf55b1887818b15cd3b594f555d249e2db'))
+		if E:A.append(('https://github.com/TechnoIndian/Tools/releases/download/Tools/Bak_Smali.jar',B.baksmali,'418b3861efc0f3afffb10d58a244e848d34f86cbdd3261ad2a2d1d57ee663766'))
+		if C or D or E:A.append(('https://github.com/TechnoIndian/Tools/releases/download/Tools/Axml2Xml.jar',B.Axml2Xml_Path,'e3a09af1255c703fc050e17add898562e463c87bb90c085b4b4e9e56d1b5fa62'))
 		if A:B.download_file(A)
